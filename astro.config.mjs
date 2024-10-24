238b15bfb801b4b134979722fb2db1daccc02156
Dorian <d.werner@ampeersenergy.de>
2024-10-25 01:05:25 +0200

    feat: add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://localhost:4321',
+  site: 'https://DorianGrey11.github.io',
+  base: 'kleinrath',
   integrations: [tailwind(), sitemap()]
 });
